20a807e28020313af88a2651f256d1e258d24bf4
GokberkUnal <58752103+GokberkUnal@users.noreply.github.com>
2021-11-01 21:00:19 +0300

    Initial Commit

---
+++ swift_hello_world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_hello_world
+//
+//  Created by Orhan Gökberk Ünal on 1.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_hello_world/swift_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_hello_worldApp.swift
+//  swift_hello_world
+//
+//  Created by Orhan Gökberk Ünal on 1.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
